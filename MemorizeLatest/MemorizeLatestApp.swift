fc87791cbadfb7e076936d6a0477ade9be485335
homuhomy <74242263+homuhomy@users.noreply.github.com>
2022-07-07 02:35:46 +0800

    Initial Commit

---
+++ MemorizeLatest/MemorizeLatestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeLatestApp.swift
+//  MemorizeLatest
+//
+//  Created by Nur Anati Nabilah on 07/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeLatestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
